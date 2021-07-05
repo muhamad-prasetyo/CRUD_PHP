37559a4f8d4657a9f73a45b141ecb0128b5369f8
Muhamad Prasetyo <muhamad.mprasetyo@gmail.com>
2021-07-05 17:03:11 +0700

    menambahkan file update.php

--- edit.php
+++ edit.php
@@ -20,5 +20,45 @@
 
     <h3 class="judul_table">Edit Data</h3>
 
+    <?php 
+    include 'koneksi.php';
+
+    $id = $_GET['id'];
+    $query_mysql = mysqli_query($connect, "SELECT * FROM user WHERE id='$id'")or die(mysqli_error());
+    $nomor = 1;
+    while($data = mysqli_fetch_array($query_mysql)) {
+    ?>
+
+    <form action="update.php" method="POST">
+        <table>
+            <tr>
+                <td>Nama</td>
+                <td>
+                    <input type="hidden" name="id" value="<?php echo $data['id']?>">
+                    <input type="text" name="nama" value="<?= $data['nama']?>">
+                </td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td>
+                    <input type="text" name="alamat" value="<?= $data['alamat'] ?>">
+                </td>
+            </tr>
+            <tr>
+                <td>Pekerjaan</td>
+                <td>
+                    <input type="text" name="pekerjaan" value="<?= $data['pekerjaan'] ?>">
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <input type="submit" value="Simpan">
+                </td>
+            </tr>
+        </table>
+    </form>
+
+    <?php } ?>
 </body>
 </html>
---
+++ update.php
@@ -0,0 +1,9 @@
+<?php 
+
+include 'koneksi.php';
+
+$id = $_POST['id'];
+$nama = $_POST['nama'];
+$alamat = $_POST['alamat'];
+$pekerjaan = $_POST['pekerjaan'];
+
