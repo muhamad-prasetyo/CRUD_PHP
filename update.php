b0bc2e94ea8f8bee8793ed68569866c1dcdfae5f
Muhamad Prasetyo <muhamad.mprasetyo@gmail.com>
2021-07-06 06:25:08 +0700

    selesai dalam fitur update

--- update.php
+++ update.php
@@ -7,3 +7,9 @@ $nama = $_POST['nama'];
 $alamat = $_POST['alamat'];
 $pekerjaan = $_POST['pekerjaan'];
 
+// untuk mengupdate data dengan query berikut 
+mysqli_query($connect, "UPDATE user SET nama='$nama', alamat='$alamat', pekerjaan='$pekerjaan' WHERE  id='$id'");
+
+
+// dan alihkan atau direcet kehalaman index.php kembali sekaligus dengan mengirimkan pesan updatenya 
+header("Location:index.php?pesan=update");
